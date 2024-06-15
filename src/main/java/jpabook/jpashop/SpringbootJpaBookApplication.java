3baac697ba16785391dfc9a2eb04fff919a7ac18
jihee99 <oosoojh31@gmail.com>
2024-06-15 19:11:16 +0900

    init

---
+++ src/main/java/jpabook/jpashop/SpringbootJpaBookApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaBookApplication.class, args);
+	}
+
+}
