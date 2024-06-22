5d2a215057bf976627355d65d3d86cdefacd368c
jihee99 <oosoojh31@gmail.com>
2024-06-22 19:37:39 +0900

    feat: 상품 재고 관련 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,10 @@
 package jpabook.jpashop.domain.item;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aspectj.weaver.ast.Not;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
@@ -7,6 +12,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,7 +29,27 @@ public abstract class Item {
 	@Column(name = "item_id")
 	private Long id;
 
+	private String name;
 	private int price;
 	private int stockQuantity;
+
+	@ManyToMany(mappedBy = "items")
+	private List<Category> categories = new ArrayList<>();
+
+
+	/*============= 비즈니스 로직 =============*/
+	// 재고 증가
+	public void addStock(int quantity){
+		this.stockQuantity += quantity;
+	}
+
+	// 재고 감소
+	public void removeStock(int quantity){
+		int resStock = this.stockQuantity - quantity;
+		if(resStock < 0){
+			throw new NotEnoughStockException("need more stock!");
+		}
+		this.stockQuantity = resStock;
+	}
 
 }
