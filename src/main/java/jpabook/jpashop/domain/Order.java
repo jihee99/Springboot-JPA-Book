17ce3f6cfdb2f8fb5f50de365cb03f39a1a8148f
jihee99 <oosoojh31@gmail.com>
2024-06-22 19:28:00 +0900

    feat: 주문 vo 수정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.domain;
 
+import static jakarta.persistence.FetchType.*;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -52,32 +54,4 @@ public class Order {
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
-	//==연관관계 메서드==//
-	public void setMember(Member member) {
-		this.member = member;
-		member.getOrders().add(this);
-	}
-
-	public void addOrderItem(OrderItem orderItem) {
-		orderItems.add(orderItem);
-		orderItem.setOrder(this);
-	}
-
-	public void setDelivery(Delivery delivery) {
-		this.delivery = delivery;
-		delivery.setOrder(this);
-	}
-
-	//==생성 메서드==//
-	public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
-		Order order = new Order();
-		order.setMember(member);
-		order.setDelivery(delivery);
-		for (OrderItem orderItem : orderItems) {
-			order.addOrderItem(orderItem);
-		}
-		order.setStatus(OrderStatus.ORDER);
-		order.setOrderDate(LocalDateTime.now());
-		return order;
-	}
 }
